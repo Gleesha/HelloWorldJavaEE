cf21438370e695748c9973f5d698bbef6b7ae7a0
gleesha <gleesha1993@yahoo.co.in>
2019-03-13 19:52:29 +0530

    Committing

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -2,6 +2,7 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import com.qaagility.javaee.*;
  
 public class CalcTest {
 
@@ -29,11 +30,10 @@ public class CalcTest {
 	
 	@Test
 	public void testCal(){
-		Calcmul testcalmul = new Calcmul();
-		Calculator testcaladd = new Calculator();
-		
-		assertEquals(testcalmul.mul(),18);
-		assertEquals(testcaladd.add(),9);
+	Calcmul testcalmul = new Calcmul();
+	Calculator testcaladd = new Calculator();
+	assertEquals(testcalmul.mul(),18);
+	assertEquals(testcaladd.add(),9);
 	}
 	
     }
